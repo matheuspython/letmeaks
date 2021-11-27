989da20aad699c0025a4441a992a355f0c8d4331
matheuspython <ruivorobado@gmail.com>
2021-11-27 00:23:12 -0300

    adicionando imgs

--- src/components/Botton.tsx
+++ src/components/Botton.tsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 
 
 
-export const Button = () => {
-  const [counter, setCounter] = useState(0)
+export const Button:React.FC = () => {
+  const [counter, setCounter] = useState(0);
 
   function increment(){
-     setCounter(counter+1)
-  }
+     setCounter(counter+1);
+  };
   return (
   <button onClick={increment}>
     {counter}
   </button>
   )
-}
+};
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -16,7 +16,7 @@ const firebaseConfig = {
 /*
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  authDomai/n: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
