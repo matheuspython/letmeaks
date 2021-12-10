9003a825785d9e3575610d51dc229a4099011ca1
matheuspython <ruivorobado@gmail.com>
2021-12-09 22:32:15 -0300

    iniciando dia 3

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,22 +1,46 @@
 import { useParams } from 'react-router-dom'
 import logoImg from '../assets/images/logo.svg'
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 
 import { Container } from '../styles/RoomStyles'
 import { Button } from '../components/Botton'
 import { RoomCode } from '../components/RoomCode'
+import { useAuth } from '../hooks/useAuths'
+import { database } from '../services/firebase'
 type RoomParams = {
   id: string;
 }
 export function Room() {
+  const [newQuestion, setNewQuestion] = useState('')
   const params = useParams<RoomParams>()
+  const roomId = params.id;
+  const {user} = useAuth()
+  async function handleSendQuestion(event: FormEvent){
+    event.preventDefault()
+    if(newQuestion.trim() === ''){
+      return
+    }
+    if(!user){
+      throw new Error("You must be logged in")
+    }
+    const question = {
+      content: newQuestion,
+      author:{
+        name: user.name,
+        avatar: user.avatar
+      },
+      isHighLighted: false,
+      isAnswered: false
+    }
+    await database.ref(`rooms/${roomId}/questions`).push(question)
+  }
 
   return (
     <Container id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="logo" />
-          <RoomCode code={params.id}/>
+          <RoomCode code={roomId}/>
 
 
         </div>
@@ -26,13 +50,14 @@ export function Room() {
           <h1>sala React</h1>
           <span>4 perguntas</span>
         </div>
-        <form action="">
+        <form onSubmit={handleSendQuestion}>
           <textarea 
             placeholder="O que voce quer perguntar?"
+            onChange={event => setNewQuestion(event.target.value)}
           />
           <div className="form-footer">
             <span>para enviar uma pergunta <button>faça seu login</button></span>
-            <Button type="submit">Enviar pergunta</Button>
+            <Button disabled={!user} type="submit">Enviar pergunta</Button>
           </div>
         </form>
       </main>
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -13,18 +13,6 @@ const firebaseConfig = {
   appId: "1:84307817907:web:ce1a258ca11cf3bfb5c7c6"
 };
 
-/*
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomai/n: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
-};
-*/
-
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
